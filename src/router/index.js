6f9fee3cb204504af60be0be079ebf016b446488
Feyyaz Balcı <feyyazbalciceng@gmail.com>
2022-08-18 22:30:49 +0300

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import YapilacakEkle from '../views/YapilacakEkle.vue'
+import YapilacakGuncelle from '../views/YapilacakGuncelle.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/yapilacak-ekle',
+    name: 'YapilacakEkle',
+    component: YapilacakEkle
+  },
+  {
+    path: '/yapilacak-guncelle/:id',
+    name: 'YapilacakGuncelle',
+    component: YapilacakGuncelle,
+    props:true
   }
 ]
 
